293d8db5cb35d3809d577482ef7d7d3784ac1cbd
aliamer0 <work.aliamer@gmail.com>
2025-01-02 20:06:41 +0200

    Initial commit, adding scraper class stub

---
+++ Scraper.py
@@ -0,0 +1,11 @@
+import time
+from datetime import datetime
+from decimal import Decimal
+import mysql.connector
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+class Scraper:
+    def __init__(self, ):
