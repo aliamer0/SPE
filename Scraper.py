b60a30493b58047a9546c85bf14c9f21c4697648
aliamer0 <work.aliamer@gmail.com>
2025-01-02 21:54:36 +0200

    added main method to populate database and auxiliary methods

--- Scraper.py
+++ Scraper.py
@@ -6,6 +6,30 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+import re
+
+
+"""
+    Database table movies scheme
+
++-------------------+---------------+------+-----+---------+----------------+
+| Field             | Type          | Null | Key | Default | Extra          |
++-------------------+---------------+------+-----+---------+----------------+
+| id                | int(11)       | NO   | PRI | NULL    | auto_increment |
+| title             | varchar(255)  | NO   |     | NULL    |                |
+| release_date      | date          | YES  |     | NULL    |                |
+| poster            | longblob      | YES  |     | NULL    |                |
+| url               | varchar(2084) | NO   |     | NULL    |                |
+| rating            | decimal(2,1)  | NO   |     | NULL    |                |
+| number_of_ratings | int(20)       | NO   |     | NULL    |                |
+| duration          | time          | NO   |     | NULL    |                |
+| plot              | text          | YES  |     | NULL    |                |
+| trailer_file      | varchar(50)   | YES  |     | NULL    |                |
+| narration         | text          | YES  |     | NULL    |                |
++-------------------+---------------+------+-----+---------+----------------+
+
+"""
+
 
 class Scraper:
     def __init__(self, host, user, password, database, user_agent):
@@ -37,7 +61,7 @@ class Scraper:
                         {columns}
                         VALUES (%s, %s, %s, %s, %s);"""
         
-        self.cursor.execute(insert_query)
+        self.cursor.execute(insert_query, values)
         self.conn.commit()
 
     def db_delete(self, table, condition)
@@ -53,4 +77,85 @@ class Scraper:
                                 WHERE {condition};"""
             self.cursor.execute(insert_query)
             self.conn.commit()
+
+    def quit(self):
+        self.cursor.close()
+        self.conn.close()
+        self.driver.quit()
+
+    def navigate(self, url):
+        self.driver.get(url)
+
+    def find_element_by(self, method, value):
+        return self.driver.find_element(By.method, value)
+
+
+    #disclaimer this project and intended web scraping acts are for educational puprposes only
+    #and not intended for any commercial use :)
+    def populate_database_movies(self):
+        """ Populate database with title, url, rating, number_of_ratings, duration, etc """
+        self.navigate("https://www.imdb.com/search/title/?title_type=feature&user_rating=6,&num_votes=10000,")
+        ul = self.find_element_by(XPATH, '//*[@id="__next"]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[2]/ul')
+        button = self.find_element_by(XPATH, '//*[@id="__next"]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[2]/div[2]/div/span/button')
+
+        for i in range(175):
+            current_scroll_position = self.driver.execute_script("return window.scrollY;")
+            self.driver.execute_script(f"window.scrollTo(0, {current_scroll_position + 10000});")
+
+            if i == 0:
+                time.sleep(10)
+            else:
+                time.sleep(10)
+            try:
+                button.click()
+            except Exception as e:
+                print("Reached the end")
+            time.sleep(10)
+
+
+        lis = ul.find_element(By.TAG_NAME, 'li')
+
+        time.sleep(10)
+        for i in range(1, 8788):
+            title_element = self.find_element_by(XPATH, f'//*[@id="__next"]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[2]/ul/li[{i}]/div/div/div/div[1]/div[2]/div[1]/a/h3')
+            title = title_element.text
+            title = title[3:]
+            url_element = self.find_element_by(XPATH, f'//*[@id="__next"]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[2]/ul/li[{i}]/div/div/div/div[1]/div[2]/div[1]/a')
+            url_text = url_element.get_attribute('href')
+            url_aux = ""
+            for j in url_text:
+                if j == "?":
+                    break
+                else:
+                    url_aux += j
+            url = url_aux
+            rating_element_xpath = f'//*[@id="__next"]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[2]/ul/li[{i}]/div/div/div/div[1]/div[2]/span/div/span/span[1]'
+            rating_element = self.find_element_by(XPATH, rating_element_xpath)
+            rating = Decimal(rating_element.text)
+            number_of_ratings_element = self.find_element_by(XPATH, f'//*[@id="__next"]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[2]/ul/li[{i}]/div/div/div/div[1]/div[2]/span/div/span/span[2]')
+            number_of_ratings = number_of_ratings_element.text
+            if number_of_ratings[-2] == "K":
+                number_of_ratings = int(number_of_ratings[2:-2]) * 1000
+            elif number_of_ratings[-2] == "M":
+                number_of_ratings = float(number_of_ratings[2:-2]) * 1000000
+                number_of_ratings = int(number_of_ratings)
+                
+            duration_element = self.find_element_by(XPATH, f'//*[@id="__next"]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[2]/ul/li[{i}]/div/div/div/div[1]/div[2]/div[2]/span[2]')
+            duration_text = duration_element.text
+            if duration_text[1] == 'h':
+                if len(duration_text) == 5:
+                    duration = "0" + duration_text[0] + ":" + "0" + duration_text[3] + ":00"
+                else:
+                    duration = "0" + duration_text[0] + ":" + duration_text[3] + duration_text[4] + ":00"
+            else:
+                duration = "00:" + duration_text[0:2] + ":00"
+
+            duration = datetime.strptime(duration, "%H:%M:%S").time()
+            values = (title, url, rating, number_of_ratings, duration)        
+            self.db_insert( "movies", "(title, url, rating, number_of_ratings, duration)", values)
+
+        
+
+                
+
 
